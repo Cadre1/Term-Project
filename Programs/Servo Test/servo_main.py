75197e744aeb02c213282ee4e7229fd81762ab88
Cadre1 <cadre200@gmail.com>
2024-03-12 19:06:28 -0700

    Functioning Servo Driver!

--- Programs/Servo Test/servo_main.py
+++ Programs/Servo Test/servo_main.py
@@ -15,14 +15,14 @@ class Servo:
             self.pin = pyb.Pin(pin, pyb.Pin.OUT_PP)
             self.pin.low()
             self.zero = zero_angle
-            self.PWM_tim = timer.channel(1, mode=pyb.Timer.PWM, pin=self.pin, freq=100)
+            self.PWM_tim = timer.channel(1, mode=pyb.Timer.PWM, pin=self.pin)
             if zero_angle > 270:
-                raise Exception("zero_angle larger than 270")
+                raise ValueError("zero_angle larger than 270")
             elif zero_angle < 0:
-                raise Exception("zero_angle must be positive")
-            self.PWM_tim.pulse_width_percent(zero_angle*100/270)
+                raise ValueError("zero_angle must be positive")
+            self.PWM_tim.pulse_width(int(zero_angle*(2500-500)/(270)+500))
             print("Servo class created, servo set to zero_angle")
-        except Exception as e:
+        except ValueError as e:
             print('Error, Servo driver failed in initialization')
             print(e)
     
@@ -36,7 +36,7 @@ class Servo:
                 raise Exception("angle larger than 270")
             elif angle < 0:
                 raise Exception("angle must be positive")
-                self.PWM_tim.pulse_width_percent(angle*100/270)
+            self.PWM_tim.pulse_width(int(angle*(2500-500)/(270)+500))
         except Exception as e:
             print("Error executing SetAngle")
             print(e)
@@ -51,29 +51,27 @@ class Servo:
                 raise Exception("deflection exceeds motor limits")
             elif dangle + self.zero < 0:
                 raise Exception("deflection exceeds motor limits")
-            self.PWM_tim.pulse_width_percent((dangle + self.zero)*100/270)
+            self.PWM_tim.pulse_width(int((dangle + self.zero)*(2500-500)/(270)+500))
         except Exception as e:
             print("Error executing SetDeflection")
             print(e)
 
 
 if __name__ == "__main__":
-    servo_pin = pyb.Pin(pyb.Pin.board.PA0, pyb.Pin.OUT_PP)
-    s_timer = pyb.Timer(5, freq=100)
-    my_servo = Servo(pin=servo_pin, timer=s_timer, zero_angle=100)
+    servo_pin = pyb.Pin(pyb.Pin.board.PA8, pyb.Pin.OUT_PP)
+    s_timer = pyb.Timer(1, prescaler=79, period=19999)
+    my_servo = Servo(pin=servo_pin, timer=s_timer, zero_angle=0)
     try:
         while True:
             my_servo.SetAngle(0)
             utime.sleep(2)
             my_servo.SetAngle(90)
             utime.sleep(2)
-            my_servo.SetAngle(-90)
-            utime.sleep(2)
-            my_servo.SetDeflection(0)
-            utime.sleep(2)
-            my_servo.SetDeflection(90)
-            utime.sleep(2)
-            my_servo.SetDeflection(-90)
-            utime.sleep(2)
+            #my_servo.SetDeflection(0)
+            #utime.sleep(2)
+            #my_servo.SetDeflection(90)
+            #utime.sleep(2)
+            #my_servo.SetDeflection(-90)
+            #utime.sleep(2)
     except KeyboardInterrupt:
         print("stopped the code")
